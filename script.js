b7e426687ad6a32a772cb4212572f5e7693eab61
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-17 22:47:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
+const continueBtn = document.getElementById("continueBtn");
+const usernameInput = document.getElementById("username");
+const personalText = document.getElementById("personalText");
 const scenes = document.querySelectorAll(".scene");
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
 
-window.addEventListener("scroll", () => {
-  scenes.forEach(scene => {
-    const rect = scene.getBoundingClientRect();
-    if (rect.top < window.innerHeight * 0.6 &&
-        rect.bottom > window.innerHeight * 0.3) {
-      scene.classList.add("active");
-    } else {
-      scene.classList.remove("active");
-    }
-  });
+continueBtn.addEventListener("click", () => {
+  const name = usernameInput.value || "Traveler";
+
+  scenes[0].classList.remove("active");
+  scenes[1].classList.add("active");
+
+  personalText.innerHTML = `
+    ${name}…<br><br>
+    tum adhure isliye nahi ho<br>
+    kyunki tumhare paas kuch kam hai,<br><br>
+    balki isliye…<br>
+    kyunki tum gehra mehsoos karte ho.
+  `;
+});
+
+musicBtn.addEventListener("click", () => {
+  music.play();
+  scenes[1].classList.remove("active");
+  scenes[2].classList.add("active");
 });
