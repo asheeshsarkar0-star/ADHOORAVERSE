fc8957fa7da2f3af543127e40997e8f1e9177a97
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-18 16:59:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
+const loader = document.getElementById("loader");
 const entry = document.getElementById("entry");
+const nameScreen = document.getElementById("nameScreen");
 const story = document.getElementById("story");
 const final = document.getElementById("final");
 
-document.getElementById("enterBtn").addEventListener("click", () => {
-  entry.classList.remove("active");
-  story.classList.add("active");
-});
+const enterBtn = document.getElementById("enterBtn");
+const continueBtn = document.getElementById("continueBtn");
 
-let step = 0;
+const username = document.getElementById("username");
+const finalText = document.getElementById("finalText");
+
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+
+setTimeout(() => {
+  loader.style.display = "none";
+  entry.classList.remove("hidden");
+}, 2000);
+
+enterBtn.onclick = () => {
+  entry.classList.add("hidden");
+  nameScreen.classList.remove("hidden");
+};
+
+continueBtn.onclick = () => {
+  const name = username.value || "Traveler";
+  nameScreen.classList.add("hidden");
+  story.classList.remove("hidden");
+
+  finalText.innerHTML = `
+    ${name}…<br><br>
+    tum adhure isliye nahi ho,<br>
+    kyunki tumhare paas kuch kam hai,<br><br>
+    balki isliye…<br>
+    kyunki tum gehra mehsoos karte ho.
+  `;
+};
+
+let scrollCount = 0;
 
 window.addEventListener("wheel", () => {
-  if (!story.classList.contains("active")) return;
-
-  step++;
-
-  if (step > 4) {
-    story.classList.remove("active");
-    final.classList.add("active");
+  if (!story.classList.contains("hidden")) {
+    scrollCount++;
+    if (scrollCount > 3) {
+      story.classList.add("hidden");
+      final.classList.remove("hidden");
+    }
   }
 });
+
+musicBtn.onclick = () => {
+  music.paused ? music.play() : music.pause();
+};
